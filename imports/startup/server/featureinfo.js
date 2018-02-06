a11586e184f6541244444361891bac61f2789ca3
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2018-02-06 15:40:46 +0100

    added 2 attributes to the result

--- imports/startup/server/featureinfo.js
+++ imports/startup/server/featureinfo.js
@@ -17,10 +17,12 @@ Meteor.methods({
 			var terrein = getValueCheck(featureObject['app:terrein']);
 			var monumentaal = getValueCheck(featureObject['app:monumentaal']);
 			var waardevol = getValueCheck(featureObject['app:waardevol']);
+			var aanvMonumentaal = getValueCheck(featureObject['app:aanv_mon']);
+			var aanvWaardevol = getValueCheck(featureObject['app:aanv_waa']);
 			
 			return {'paspoortnummer': paspoortnummer, 'straatnaam': straatnaam, 'woonplaats': woonplaats, 'boomsoort': boomsoort,
 							'aantal': aantal, 'aanlegjaar': aanlegjaar, 'terrein': terrein, 'monumentaal': monumentaal,
-							'waardevol': waardevol};
+							'waardevol': waardevol, 'aanvMonumentaal': aanvMonumentaal, 'aanvWaardevol': aanvWaardevol};
 		}
 		
 		function getValueCheck(field) {
