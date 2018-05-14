10978f1cac32da6aee9720456038a8abd2bc85c3
Sandro Neumann <sandro.neumann@idgis.nl>
2018-05-14 15:22:44 +0200

    Fix bug addition 'monumentaal' & 'waarde'

--- imports/startup/server/featureinfo.js
+++ imports/startup/server/featureinfo.js
@@ -8,28 +8,30 @@ Meteor.methods({
 		if(typeof xml.FeatureCollection['gml:featureMember'] !== 'undefined') {
 			var featureObject = xml.FeatureCollection['gml:featureMember'][0]['app:paspoortbomen_sittard_geleen'][0];
 			
-			var paspoortnummer = getValueCheck(featureObject['app:paspoort']);
-			var straatnaam = getValueCheck(featureObject['app:straatnaam']);
-			var woonplaats = getValueCheck(featureObject['app:wpl_wijk']);
-			var boomsoort = getValueCheck(featureObject['app:boomsoort']);
-			var aantal = getValueCheck(featureObject['app:aantal']);
-			var aanlegjaar = getValueCheck(featureObject['app:aanlegjaar']);
-			var terrein = getValueCheck(featureObject['app:terrein']);
-			var monumentaal = getValueCheck(featureObject['app:monumentaal']);
-			var waardevol = getValueCheck(featureObject['app:waardevol']);
-			var aanvMonumentaal = getValueCheck(featureObject['app:aanvulling_monumentaal']);
-			var aanvWaardevol = getValueCheck(featureObject['app:aanvulling_waardevol']);
+			var paspoortnummer = getValueCheck(featureObject['app:paspoort'], true);
+			var straatnaam = getValueCheck(featureObject['app:straatnaam'], true);
+			var woonplaats = getValueCheck(featureObject['app:wpl_wijk'], true);
+			var boomsoort = getValueCheck(featureObject['app:boomsoort'], true);
+			var aantal = getValueCheck(featureObject['app:aantal'], true);
+			var aanlegjaar = getValueCheck(featureObject['app:aanlegjaar'], true);
+			var terrein = getValueCheck(featureObject['app:terrein'], true);
+			var monumentaal = getValueCheck(featureObject['app:monumentaal'], true);
+			var waardevol = getValueCheck(featureObject['app:waardevol'], true);
+			var aanvMonumentaal = getValueCheck(featureObject['app:aanvulling_monumentaal'], false);
+			var aanvWaardevol = getValueCheck(featureObject['app:aanvulling_waardevol'], false);
 			
 			return {'paspoortnummer': paspoortnummer, 'straatnaam': straatnaam, 'woonplaats': woonplaats, 'boomsoort': boomsoort,
 							'aantal': aantal, 'aanlegjaar': aanlegjaar, 'terrein': terrein, 'monumentaal': monumentaal,
 							'waardevol': waardevol, 'aanvMonumentaal': aanvMonumentaal, 'aanvWaardevol': aanvWaardevol};
 		}
 		
-		function getValueCheck(field) {
-			if(typeof field != 'undefined') {
+		function getValueCheck(field, getLabelUndefined) {
+			if(field != undefined) {
 				return field[0];
 			} else {
-				return 'onbekend';
+				if(getLabelUndefined) {
+					return 'onbekend';
+				}
 			}
 		}
 	}
